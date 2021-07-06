b3c17a31a86ce989c06e1b8fb6bea26d9ebf16c8
gourav028 <87014518+gourav028@users.noreply.github.com>
2021-07-06 16:31:01 +0530

    Day 1 Assignments Submitted

---
+++ LetsUpgradeAssignment1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  5 20:24:03 2021
+
+@author: Gourav
+"""
+
+#Assignment1
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.arange(0,10)
+y=x*x
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.plot(x,y)
+plt.show()
---
+++ LetsUpgradeAssignment2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  5 20:37:14 2021
+
+@author: Gourav
+"""
+#Assignment2
+import matplotlib.pyplot as plt
+import numpy as np
+x=[1,2,3,4,5,6,7]#DaysOfWeek
+y=[160,150,140,145,175,165,180]#Sales
+plt.xlabel("Days Of Week")
+plt.ylabel("Sales")
+plt.plot(x,y)
+plt.show()
